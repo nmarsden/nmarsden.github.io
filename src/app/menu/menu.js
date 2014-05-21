8c14359a8d69a01d998ee12acdd6e5289fd47b25
nmarsden <nmarsden@gmail.com>
2014-05-21 22:52:35 +1000

    increased available moves from 10 to 15

--- src/app/menu/menu.js
+++ src/app/menu/menu.js
@@ -1,23 +1,22 @@
-angular.module('meg.menu', [
+angular.module( 'meg.menu', [
   'ui.router',
   'ui.bootstrap'
-]).config([
-  '$stateProvider',
-  function config($stateProvider) {
-    $stateProvider.state('menu', {
-      url: '/menu',
-      views: {
-        'main': {
-          controller: 'MenuCtrl',
-          templateUrl: 'menu/menu.tpl.html'
-        }
-      },
-      data: { pageTitle: 'Menu' }
-    });
-  }
-]).controller('MenuCtrl', [
-  '$scope',
-  function MenuCtrl($scope) {
-  }
-]);
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'menu', {
+    url: '/menu',
+    views: {
+      "main": {
+        controller: 'MenuCtrl',
+        templateUrl: 'menu/menu.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Menu' }
+  });
+})
+
+.controller( 'MenuCtrl', function MenuCtrl( $scope ) {
+})
+
 ;
