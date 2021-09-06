73b161373bd186f870f3f3be6f527cf8d3b8348b
Neil Marsden <neil.marsden@oracle.com>
2021-09-06 19:49:50 +1000

    - shrink banner on scrolling down

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,9 +3,11 @@ import style from './style.css';
 
 const Banner: FunctionalComponent = () => {
     return (
-      <div className={style.banner}>
-          <div className={style.heading}>Web Projects</div>
-          <div className={style.subHeading}>Crafted by Neil Marsden</div>
+      <div className={style.bannerWrapper}>
+        <div className={style.banner}>
+            <div className={style.heading}>Web Projects</div>
+            <div className={style.subHeading}>Crafted by Neil Marsden</div>
+        </div>
       </div>
     );
 };
