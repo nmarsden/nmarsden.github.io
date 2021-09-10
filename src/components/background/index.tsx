62a5b9889889799470552e97043782d2ae60fe6f
Neil Marsden <neil.marsden@oracle.com>
2021-09-10 17:57:51 +1000

    - tweak background/header/footer styles - refactored <Background>

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,19 +1,33 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
 
+type AnimationType = 'move' | 'move-large';
+type Animation = {
+    duration: number;
+    type: AnimationType;
+};
+
+const CIRCLES: Animation[] = [
+    { duration: 10, type: 'move' },
+    { duration: 17, type: 'move-large' },
+    { duration:  7, type: 'move' },
+    { duration:  9, type: 'move' },
+    { duration: 11, type: 'move' },
+    { duration: 15, type: 'move-large' },
+    { duration: 10, type: 'move' },
+    { duration: 16, type: 'move-large' },
+    { duration:  8, type: 'move' },
+];
+
 const Background: FunctionalComponent = () => {
     return (
       <div className={style.backgroundContainer}>
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
-        <div className={style.backgroundCircle} />
+          { CIRCLES.map((circle, index) => {
+            const animationName = circle.type === 'move' ? style.backgroundCircleMove : style.backgroundCircleMoveLarge;
+            return <div key={index}
+                   className={style.backgroundCircle}
+                   style={`animation-delay: -${index}s; animation-duration: ${circle.duration}s; animation-name: ${animationName}`} />
+          })}
       </div>
     );
 };
